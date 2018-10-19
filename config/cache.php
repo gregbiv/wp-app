53c9b853a55751e9be42af7fa3ad61cff72b64cf
Gregory Kornienko <gregbiv@gmail.com>
2018-10-19 16:46:25 +0200

    Enable multisite

--- config/cache.php
+++ config/cache.php
@@ -3,6 +3,12 @@
 /**
  * Redis settings
  */
+/* Redis cache enabled */
+defined('WP_REDIS_DISABLED')     or define('WP_REDIS_DISABLED', filter_var(
+    getenv('WP_REDIS_DISABLED'),
+    FILTER_VALIDATE_BOOLEAN
+));
+
 /* Redis host */
 defined('WP_REDIS_HOST')     or define('WP_REDIS_HOST', getenv('WP_REDIS_HOST'));
 
